efcb0315e5bced521778692d3fbd514424be90a9
vimal patel <vimal.patel.nvs@gmail.com>
2020-04-15 23:26:52 +0530

    Terminal message enhancement

--- bin/create-app.js
+++ bin/create-app.js
@@ -4,6 +4,8 @@ const chalk = require("chalk");
 const program = require('commander');
 
 console.log(chalk.yellow(figlet.textSync('reactjs-cli', { horizontalLayout: 'full' })));
+console.log(chalk.white.bgRedBright('version:- 1.0.0'));
+console.log(chalk.white.bgRedBright('version:- 1.0.0'));
 program
   .version('0.0.1')
   .description("An example CLI for ordering pizza's")
@@ -13,3 +15,4 @@ program
   .option('-c, --cheese <type>', 'Add the specified type of cheese [marble]')
   .option('-C, --no-cheese', 'You do not want any cheese')
   .parse(process.argv);
+
