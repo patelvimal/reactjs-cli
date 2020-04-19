5b71238ddd19bc082c00481a73a10007c235752e
vimal patel <vimal.patel.nvs@gmail.com>
2020-04-19 09:04:07 +0530

    Terminal text updated

--- bin/react-cli.js
+++ bin/react-cli.js
@@ -3,42 +3,38 @@ var figlet = require('figlet');
 const chalk = require("chalk");
 const program = require('commander');
 const app = require('../lib/app/app');
+const version = require('../lib/version');
 
 console.log(chalk.yellow(figlet.textSync('reactApp-cli', { horizontalLayout: 'full' })));
-//console.log(chalk.whiteBright('A CLI for creating react project/components'));
-
-//const version = chalk.whiteBright('version:- 1.0.0');
-//console.log(version);
-
-const helpText = `
-    How to Use:-
-    rx [command] -option
-
-    Example:-
-    rx new -a <For creating new application>
-    rx new -c <For creating new component>
-    rx new -s <For creating new service>
-`;
-
-//console.log(helpText)
+console.log(chalk.yellow('A CLI for creating react project/components.'));
+console.log('');
 
 program
-  .version('1.0.0')
-  .description('A CLI for creating react project/components');
+  .option('-v, --version', 'output the version number', version)
 
 program
-  //.option('-a, --application', 'Add Application')
+  .name('react-cli')
+  .usage('[command] [arguments]')
+program
   .command('new <name>')
-  //.alias('-a')
   .description('Create a new React Application')
   .action((name) => {
     createApp(name);
-  });
+});
 
+program
+  .command('add <name>')
+  .description('Add a new component into React Application')
+  .action((name) => {
+    addComponent(name);
+});
 
 program.parse(process.argv);
 
-
 function createApp(name){
   app.createApp(name);
+}
+
+function addComponent(name){
+  app.createComponent(name);
 }
