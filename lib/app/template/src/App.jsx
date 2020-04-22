e9e84685d31839b10d32691d8a4c44433d781bd2
vimal patel <vimal.patel.nvs@gmail.com>
2020-04-22 11:02:49 +0530

    updated the template

--- lib/app/template/src/App.jsx
+++ lib/app/template/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import logo from'./assets/logo.png';
-import './App.css';
 
-function App() {
+const App =() => {
   return (
     <div className="App">
       <img src={logo} />
     </div>
   );
-}
+};
 
 export default App;
